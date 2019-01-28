ea682a06533bbb3d12a0ef966b0a4b522dd736a7
crossopt <s17b2_fedorkina@179.ru>
2019-01-28 23:31:54 +0300

    hw2 first attempt

---
+++ Trie/src/main/java/ru/hse/crossopt/Trie/Trie.java
@@ -0,0 +1,154 @@
+package ru.hse.crossopt.Trie;
+
+import ru.hse.crossopt.Serializable.Serializable;
+import java.io.*;
+import java.util.HashMap;
+
+/** Trie class that stores strings. */
+public class Trie implements Serializable {
+    private Node root = new Node();
+
+    private class Node {
+        private boolean isTerminal;
+        private int suffixAmount;
+        private HashMap <Character, Node> nextNode = new HashMap<>();
+
+        private Node getNext(char symbol) {
+            if (!nextNode.containsKey(symbol)) {
+                nextNode.put(symbol, new Node());
+            }
+            return nextNode.get(symbol);
+        }
+
+        private boolean existsNext(char symbol) {
+            return nextNode.containsKey(symbol);
+        }
+
+        private void serialize(ObjectOutputStream objectOutputStream) throws IOException {
+            objectOutputStream.writeBoolean(isTerminal);
+            objectOutputStream.writeInt(suffixAmount);
+            objectOutputStream.writeInt(nextNode.size());
+            for (var element : nextNode.entrySet()) {
+                objectOutputStream.writeChar(element.getKey());
+                element.getValue().serialize(objectOutputStream);
+            }
+        }
+
+        private void deserialize(ObjectInputStream objectInputStream) throws IOException {
+            isTerminal = objectInputStream.readBoolean();
+            suffixAmount = objectInputStream.readInt();
+            nextNode.clear();
+            int nextNodeSize = objectInputStream.readInt();
+            for (int i = 0; i < nextNodeSize; i++) {
+                char symbol = objectInputStream.readChar();
+                var nodeForSymbol = new Node();
+                nodeForSymbol.deserialize(objectInputStream);
+                nextNode.put(symbol, nodeForSymbol);
+            }
+        }
+    }
+
+    /**
+     * Checks if given string is in trie.
+     * Runs in O(n) time, where n is the length of the given string.
+     * @param element a string for which to check whether it is in trie.
+     * @return true if the given string is in the trie or false otherwise.
+     */
+    public boolean contains(String element) {
+        Node current = root;
+        for (char symbol : element.toCharArray()) {
+            if (!current.existsNext(symbol)) {
+                return false;
+            }
+            current = current.getNext(symbol);
+        }
+        return current.isTerminal;
+    }
+
+    /**
+     * Adds given string to trie.
+     * Runs in O(n) time, where n is the length of the given string.
+     * @param element a string to add to trie.
+     * @return true if a new element has been added or false otherwise.
+     */
+    public boolean add(String element) {
+        if (contains(element)) {
+            return false;
+        }
+        Node current = root;
+        for (char symbol : element.toCharArray()) {
+            current.suffixAmount++;
+            current = current.getNext(symbol);
+        }
+        current.suffixAmount++;
+        current.isTerminal = true;
+        return true;
+    }
+
+    /**
+     * Removes given string from trie.
+     * Runs in O(n) time, where n is the length of the given string.
+     * @param element a string to remove from trie.
+     * @return true if the string had been in the trie prior to removal or false otherwise.
+     */
+    public boolean remove(String element) {
+        if (!contains(element)) {
+            return false;
+        }
+        Node current = root;
+        for (char symbol : element.toCharArray()) {
+            current.suffixAmount--;
+            current = current.getNext(symbol);
+        }
+        current.suffixAmount--;
+        current.isTerminal = false;
+        return true;
+    }
+
+    /**
+     * Gets size of trie. Runs in O(1) time.
+     * @return amount of strings in the trie.
+     */
+    public int size() {
+        return root.suffixAmount;
+    }
+
+    /**
+     * Counts amount of strings in trie that start with given prefix.
+     * Runs in O(n) time, where n is the length of the given prefix.
+     * @param prefix a prefix for which to count amount of strings.
+     * @return amount of strings in trie that start with given prefix.
+     */
+    public int howManyStartWithPrefix(String prefix) {
+        Node current = root;
+        for (char symbol : prefix.toCharArray()) {
+            if (!current.existsNext(symbol)) {
+                return 0;
+            }
+            current = current.getNext(symbol);
+        }
+        return current.suffixAmount;
+    }
+
+    /**
+     * Writes trie into output stream.
+     * @param out an output stream to write into.
+     * @throws IOException if writing into stream failed.
+     */
+    public void serialize(OutputStream out) throws IOException {
+        try (var objectOutputStream = new ObjectOutputStream(out)) {
+            root.serialize(objectOutputStream);
+        }
+    }
+
+    /**
+     * Reads trie from input stream.
+     * @param in An input stream to read from.
+     * @throws IOException if reading from stream failed.
+     */
+    public void deserialize(InputStream in) throws IOException {
+        try (var objectInputStream = new ObjectInputStream(in)) {
+            root.deserialize(objectInputStream);
+        }
+    }
+}
---
+++ Trie/src/test/java/ru/hse/crossopt/Trie/TrieTest.java
@@ -0,0 +1,110 @@
+package ru.hse.crossopt.Trie;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrieTest {
+    Trie emptyTrie;
+    Trie testTrie;
+
+    @BeforeEach
+    void setUp() {
+        emptyTrie = new Trie();
+        testTrie = new Trie();
+        testTrie.add("dog");
+        testTrie.add("cat");
+        testTrie.add("car");
+        testTrie.add("cart");
+        testTrie.add("care");
+    }
+
+    @Test
+    void size() {
+        assertEquals(0, emptyTrie.size());
+        assertEquals(5, testTrie.size());
+    }
+
+    @Test
+    void howManyStartWithPrefix_existingPrefix() {
+        assertEquals(testTrie.howManyStartWithPrefix("ca"), 4);
+        assertEquals(testTrie.howManyStartWithPrefix("cart"), 1);
+        assertEquals(testTrie.howManyStartWithPrefix(""), 5);
+    }
+
+    @Test
+    void howManyStartWithPrefix_missingPrefix() {
+        assertEquals(emptyTrie.howManyStartWithPrefix("ca"), 0);
+        assertEquals(testTrie.howManyStartWithPrefix("me"), 0);
+        assertEquals(testTrie.howManyStartWithPrefix("cares"), 0);
+    }
+
+    @Test
+    void contains_missingElement() {
+        assertFalse(emptyTrie.contains("cats"));
+        assertFalse(testTrie.contains("cats"));
+        assertFalse(testTrie.contains("ca"));
+    }
+
+    @Test
+    void contains_existingElement() {
+        assertTrue(testTrie.contains("care"));
+        testTrie.add("cats");
+        assertTrue(testTrie.contains("cats"));
+    }
+
+    @Test
+    void add_existingElement() {
+        assertFalse(testTrie.add("care"));
+        assertFalse(testTrie.add("dog"));
+        assertEquals(5, testTrie.size());
+        assertEquals(testTrie.howManyStartWithPrefix("ca"), 4);
+    }
+
+    @Test
+    void add_missingElement() {
+        assertTrue(testTrie.add("ca"));
+        assertTrue(testTrie.add("cats"));
+        assertEquals(7, testTrie.size());
+        assertEquals(testTrie.howManyStartWithPrefix("c"), 6);
+        assertTrue(emptyTrie.add("cat"));
+        assertEquals(1, emptyTrie.size());
+    }
+
+    @Test
+    void remove_existingElement() {
+        assertTrue(testTrie.remove("care"));
+        assertTrue(testTrie.remove("car"));
+        assertEquals(testTrie.size(), 3);
+        assertEquals(testTrie.howManyStartWithPrefix("car"), 1);
+    }
+
+    @Test
+    void remove_missingElement() {
+        assertFalse(emptyTrie.remove("cat"));
+        assertFalse(testTrie.remove("c"));
+        assertEquals(5, testTrie.size());
+        assertEquals(testTrie.howManyStartWithPrefix("car"), 3);
+    }
+
+    @Test
+    void serializeAndDeserialize() throws IOException {
+        var byteArrayOutputStream = new ByteArrayOutputStream();
+        testTrie.serialize(byteArrayOutputStream);
+        var byteArrayInputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+        var serializedResult = new Trie();
+        serializedResult.deserialize(byteArrayInputStream);
+
+        assertEquals(5, serializedResult.size());
+        assertTrue(serializedResult.contains("care"));
+        assertTrue(serializedResult.contains("cart"));
+        assertTrue(serializedResult.contains("car"));
+        assertTrue(serializedResult.contains("cat"));
+        assertTrue(serializedResult.contains("dog"));
+    }
+}
