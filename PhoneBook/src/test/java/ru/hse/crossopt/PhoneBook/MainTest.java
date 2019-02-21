602cf311862789e7284ba69a7b1c546adeddf6eb
crossopt <s17b2_fedorkina@179.ru>
2019-02-21 15:17:39 +0300

    maybe this would be better with try-with-resources

--- PhoneBook/src/test/java/ru/hse/crossopt/PhoneBook/MainTest.java
+++ PhoneBook/src/test/java/ru/hse/crossopt/PhoneBook/MainTest.java
@@ -17,13 +17,14 @@ class MainTest {
     //The interesting tests are for the PhoneBook class. This just is a basic test to show interactor works correctly.
     @Test
     void test_example() throws IOException {
-        var inputFile = new FileInputStream(new File("src/test/resources/mainTest.in"));
-        var outputFile = new FileOutputStream(new File("src/test/resources/mainTest.out"));
-        System.setIn(inputFile);
-        System.setOut(new PrintStream(outputFile));
-        Main.main(new String[0]);
-        var actual = Paths.get("src/test/resources/mainTest.out");
-        var expected = Paths.get("src/test/resources/mainTest.ans");
-        assertArrayEquals(Files.readAllBytes(actual), Files.readAllBytes(expected));
+        try (var inputFile = new FileInputStream(new File("src/test/resources/mainTest.in"));
+            var outputFile = new FileOutputStream(new File("src/test/resources/mainTest.out"))) {
+            System.setIn(inputFile);
+            System.setOut(new PrintStream(outputFile));
+            Main.main(new String[0]);
+            var actual = Paths.get("src/test/resources/mainTest.out");
+            var expected = Paths.get("src/test/resources/mainTest.ans");
+            assertArrayEquals(Files.readAllBytes(actual), Files.readAllBytes(expected));
+        }
     }
 }
