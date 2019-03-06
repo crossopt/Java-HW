13dfb5b499b1231e6d5a9d91c5208cb98d0fe24e
crossopt <s17b2_fedorkina@179.ru>
2019-03-06 16:42:30 +0300

    maybe works

---
+++ Injector/src/main/java/ru/hse/crossopt/Injector/AmbiguousImplementationException.java
@@ -0,0 +1,7 @@
+package ru.hse.crossopt.Injector;
+
+public class AmbiguousImplementationException extends Exception {
+    public AmbiguousImplementationException(String message) {
+        super(message);
+    }
+}
