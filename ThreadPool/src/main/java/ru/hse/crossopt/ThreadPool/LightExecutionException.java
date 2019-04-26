d3665875ba460f0d61dd6498ecc36abe59654c7c
crossopt <s17b2_fedorkina@179.ru>
2019-04-26 17:26:26 +0300

    more annotations

--- ThreadPool/src/main/java/ru/hse/crossopt/ThreadPool/LightExecutionException.java
+++ ThreadPool/src/main/java/ru/hse/crossopt/ThreadPool/LightExecutionException.java
@@ -1,9 +1,11 @@
 package ru.hse.crossopt.ThreadPool;
 
+import org.jetbrains.annotations.NotNull;
+
 /** Exception that is thrown when exception in supplier's calculation occurs. */
 public class LightExecutionException extends Exception {
     /** Constructor that creates LightExecutionException from the exception of the supplier. */
-    public LightExecutionException(Exception exception) {
+    public LightExecutionException(@NotNull Exception exception) {
         super(exception);
     }
 }
--- ThreadPool/src/main/java/ru/hse/crossopt/ThreadPool/LightFuture.java
+++ ThreadPool/src/main/java/ru/hse/crossopt/ThreadPool/LightFuture.java
@@ -1,5 +1,8 @@
 package ru.hse.crossopt.ThreadPool;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import java.util.function.Function;
 
 /** Interface that stores the tasks that will be executed in a ThreadPool. */
@@ -12,7 +15,7 @@ public interface LightFuture<T> {
      * @return result of the execution of the task.
      * @throws LightExecutionException if exception occurred in supplier's calculation.
      */
-    T get() throws LightExecutionException;
+    @Nullable T get() throws LightExecutionException;
 
     /**
      * Applies the given function to the result of the task.
@@ -20,5 +23,5 @@ public interface LightFuture<T> {
      * @param <R> the type of the new task.
      * @return a new LightFuture task.
      */
-    <R> LightFuture<R> thenApply(Function<? super T, R> function);
+    @NotNull <R> LightFuture<R> thenApply(@NotNull Function<? super T, R> function);
 }
