0a3ede33711ca1d0d84f1189dc46c838b00d94ff
crossopt <s17b2_fedorkina@179.ru>
2019-05-29 17:24:02 +0300

    CW done, but I don't know how to launch with arguments in IDEA

---
+++ FindPairs/src/main/java/ru/hse/crossopt/findpairs/Card.java
@@ -0,0 +1,48 @@
+package ru.hse.crossopt.findpairs;
+
+import javafx.scene.control.Button;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkState;
+
+/** Class describing a card on the game board. */
+public class Card {
+    private final int number;
+    private boolean wasFound;
+    private @Nullable Button button;
+
+    /** Creates a new card with the given number on it. */
+    public Card(int number) {
+        checkArgument(number >= 0);
+        this.number = number;
+        wasFound = false;
+    }
+
+    /** Returns the card's number. */
+    public int getNumber() {
+        return number;
+    }
+
+    /** Returns true if the card has been found, false otherwise. */
+    public boolean wasFound() {
+        return wasFound;
+    }
+
+    /** Sets the card's state to found. */
+    public void setFound() {
+        this.wasFound = true;
+    }
+
+    /** Returns the button that depicts this card. */
+    public Button getButton() {
+        return button;
+    }
+
+    /** Sets the given button to depict the card. Button should not be set more than once. */
+    public void addButton(@NotNull Button button) {
+        checkState(this.button == null); //should not add a button more than once.
+        this.button = button;
+    }
+}
---
+++ FindPairs/src/test/java/ru/hse/crossopt/findpairs/GameBoardTest.java
@@ -0,0 +1,62 @@
+package ru.hse.crossopt.findpairs;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameBoardTest {
+    private GameBoard board;
+
+    @BeforeEach
+    void setUp() {
+        board = new GameBoard(6);
+    }
+
+    @Test
+    void boardGenerationIsCorrect() {
+        int cardsAmount = 18;
+        int[] neededNumbers = new int[cardsAmount];
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                neededNumbers[board.get(i, j).getNumber()]++;
+            }
+        }
+        for (int i = 0; i < cardsAmount; i++) {
+            assertEquals(2, neededNumbers[i]);
+        }
+    }
+
+    @Test
+    void badBoardSizeFails() {
+        assertThrows(IllegalArgumentException.class, () -> new GameBoard(0));
+        assertThrows(IllegalArgumentException.class, () -> new GameBoard(-5));
+        assertThrows(IllegalArgumentException.class, () -> new GameBoard(7));
+        assertThrows(IllegalArgumentException.class, () -> new GameBoard(100000));
+    }
+
+    @Test
+    void isWinForNonWinningBoard() {
+        assertFalse(board.isWin());
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (i + j > 0) {
+                    board.get(i, j).setFound();
+                    assertFalse(board.isWin());
+                }
+            }
+        }
+    }
+
+    @Test
+    void isWinForWinningBoard() {
+        assertFalse(board.isWin());
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                board.get(i, j).setFound();
+            }
+        }
+        assertTrue(board.isWin());
+        assertTrue(board.isWin());
+    }
+}
